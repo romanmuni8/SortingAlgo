f95e580b4c84bd19fb520677ea9ea4b258d0e9df
Roman Kozulia <rk8318@rit.edu>
2021-01-19 10:18:42 -0500

    Quick3 sort done

--- src/QuickSort/QuickSort3.java
+++ src/QuickSort/QuickSort3.java
@@ -7,11 +7,24 @@ public class QuickSort3 {
     private static Random random = new Random();
 
     private static int[] partition3(int[] a, int l, int r) {
-        //write your code here
-
-
-        int m1 = l;
-        int m2 = r;
+        int x = a[l];
+        int j = l;
+        for (int i = l + 1; i <= r; i++) {
+            if (a[i] <= x) {
+                j++;
+                int t = a[i];
+                a[i] = a[j];
+                a[j] = t;
+            }
+        }
+        int t = a[l];
+        a[l] = a[j];
+        a[j] = t;
+        int m2 = j;
+        while(j>0 && a[j-1] == a[m2]){
+            j--;
+        }
+        int m1 = j;
         int[] m = {m1, m2};
         return m;
     }
@@ -41,10 +54,11 @@ public class QuickSort3 {
         int t = a[l];
         a[l] = a[k];
         a[k] = t;
-        //use partition3
-        int m = partition2(a, l, r);
-        randomizedQuickSort(a, l, m - 1);
-        randomizedQuickSort(a, m + 1, r);
+//        int m = partition2(a, l, r);
+        int [] lr = partition3(a,l,r);
+
+        randomizedQuickSort(a, l, lr[0]-1);
+        randomizedQuickSort(a, lr[1] + 1, r);
     }
 
     public static void main(String[] args) {
@@ -54,6 +68,7 @@ public class QuickSort3 {
         for (int i = 0; i < n; i++) {
             a[i] = scanner.nextInt();
         }
+
         randomizedQuickSort(a, 0, n - 1);
         for (int i = 0; i < n; i++) {
             System.out.print(a[i] + " ");
